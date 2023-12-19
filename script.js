22b407fbacd76d57b4b48b974c0141deb02bc3f5
Moises Menezes <moisesmenezes53@gmail.com>
2023-12-18 23:55:19 -0300

    Create Functions of food and move snake

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 //Define HTML elements
 const board = document.getElementById("game-board");
 //Define game variables
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = "right";
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStart = false;
 
 //Draw game map, snake and food
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 //Draw Snake
@@ -34,3 +41,61 @@ function setPosition(element, position) {
 
 // testing draw function
 draw();
+
+//Draw food function
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+//moving the snake
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "right":
+      head.x++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "sup":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+  }
+  snake.unshift(head);
+  //snake.pop();
+
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    clearInterval(); // Clear past interval
+    gameInterval = setInterval(() => {
+      move();
+      draw();
+    }, gameSpeedDelay);
+  } else {
+    snake.pop();
+  }
+}
+
+// Test moving
+// setInterval(() => {
+//   move();
+//   draw();
+// }, 200);
+
+//Star Game function
+function startGame() {
+  gameStart = true; // Keep Track of running game
+  instruction;
+}
